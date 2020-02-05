e52a8c3a17a934044bd8724c95c6f7d0b130c6b5
Tony Collins <tjc@eltham-college.org.uk>
2020-02-05 15:39:47 +0000

    Added Changes

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter tjc = null; ;
+            Console.WriteLine("Hello World!");
+            try
+            {
+                tjc = new StreamWriter("C:/users/tjc/hello.txt");
+                tjc.WriteLine("sdfghjkl;");
+                tjc.WriteLine("rty");
+                tjc.Close();
+
+            }
+            catch
+            {
+                Console.WriteLine("error");
+
+            }
+
+            StreamReader tj = null; 
+
+            try
+            {
+                tj = new StreamReader("C:/users/tjc/hello.txt");
+                Console.WriteLine(tj.ReadToEnd());
+                tj.Close();
+
+            }
+            catch
+            {
+
+            }
+
+
+
+
+
+        }
+    }
+}
